4c2e84ff0249f8caa769e91fe74c1f8eeca9d1cc
JacobSCarrington <JacobCarrington101@gmail.com>
2024-02-19 16:59:12 -0500

    Implemented MapView for the home page and set it to default to the user's current location
    Next thing to implement would be a search function and a directions function using Google Places API and Google Directions API respectively

--- RouteLink-Tracker/app/home/homePage.js
+++ RouteLink-Tracker/app/home/homePage.js
@@ -1,13 +1,70 @@
-import { Button, Text, View, Image } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions, Button } from 'react-native';
+import * as Location from 'expo-location';
 
-const Home = () => {
-    return(
-        <View>
-            <Text style={{textAlign: "center"}}>Home</Text>
-        </View>   
-    )
-};
+export default function App() {
+    const [mapRegion, setMapRegion] = useState({
+        latitude: 33.93874201464112, 
+        longitude: -84.51974379133362,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    })
 
+    const mapJson = []
 
+    const [errorMsg, setErrorMsg] = useState(null);
 
-export default Home;
+    const userLocation = async () => {
+        try {
+           
+            let {status} = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied')
+                return;
+            }
+            let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
+
+            setMapRegion({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            })
+            console.log(location.coords.latitude, location.coords.longitude);
+        }
+        catch (error) {
+            console.error('Error getting location:', error);
+            setErrorMsg('Error getting location');
+        }
+    }
+
+    useEffect(() => {
+        userLocation();
+    }, [])
+  return (
+    <View style={styles.container}>
+        <MapView 
+        provider={PROVIDER_GOOGLE}
+        style={styles.map} 
+        region = {mapRegion}
+        customMapStyle={mapJson}
+        // API Key
+        >
+            <Marker coordinate={mapRegion} title = 'Marker' />
+        </MapView>
+        <Button title = 'Get Location' onPress = {userLocation} />
+        {errorMsg && <Text>{errorMsg}</Text>}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
